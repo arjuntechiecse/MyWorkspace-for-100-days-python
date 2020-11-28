1f403d7a7a6f8c15743ee97b4162f67f7c2448ca
Arjun Raj <sr.arjun3@gmail.com>
2020-11-28 15:37:55 +0000

    added another day's work!

--- main.py
+++ main.py
@@ -210,6 +210,262 @@ if(score<10 or score>90):
   print(f"Your score is {score}, you go together like coke and mentos.")
 elif(score>=40 and score<=50):
   print(f"Your score is {score}, you are alright together.")
-
 else:
   print(f"Your score is {score}.")
+
+  #DAY3 PROJECT
+  
+  print('''
+
+
+                            /  \
+                           |    |
+             _             |    |
+           /' |            | _  |
+          |   |            |    |
+          | _ |            |    |
+          |   |            |    |
+          |   |        __  | _  |
+          | _ |  __   /  \ |    |
+          |   | /  \ |    ||    |
+          |   ||    ||    ||    |       _---.
+          |   ||    ||    |. __ |     ./     |
+          | _. | -- || -- |    `|    /      //
+          |'   |    ||    |     |   /`     (/
+          |    |    ||    |     | ./       /
+          |    |.--.||.--.|  __ |/       .|
+          |  __|    ||    |-'            /
+          |-'   \__/  \__/             .|
+          |       _.-'                 /
+          |   _.-'      /             |
+          |            /             /
+          |           |             /
+          `           |            /
+           \          |          /'
+            |          `        /
+             \                .'
+             |                |
+             |                |
+             |                |
+             |                |
+''')
+print("Welcome to crush memory.")
+print("This is to find the treasure in you.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+Bridge_scene=input("You are driving a car alone early morning and you are seeing a woman fellling into the river. What will you do? Will you save her or not?\n").lower()
+
+if(Bridge_scene=="save"):
+  print("Mmm!This shows your humanity, Next\n")
+  saving_scene=input("Now you had rushed down to the bridge to save her. You had jumped into it and looked for her and saved her. Now you came to know that, She was your most favourite Crush in the college. Now will you be happy to see her or not happy?\n").lower()
+  if(saving_scene=="happy"):
+    print("Well! This tells that you still didn't forget her.\n") 
+    Guilty_scene=input("Now you saved her, Hospitalised her and felt happy too. She felt guilt by seeing you, Because she rejected your proposal while you are in the college. Since you are not job ready and she doesn't believe in you. But now she realised her mistake and tries to speak with you for joining with you again. Will you speak with her or not speak?\n").lower()
+    if(Guilty_scene=="speak"):
+      print("haha! You are good to accept her \n") 
+      joining_scene=input("She had regreted you before in college. Now Will you accept her proposal to join and marry her. Now you are well settled than she expected. Will you accept her or not? \n").lower()
+      if(joining_scene=="not accept"):
+        print("This shows your guts and manliness for rejecting her, Because she is not worth for you.\n")
+   
+else:
+  print("sorry, You are not up to the mark as expected!")
+
+
+#DAY 4
+#DAY 4.1 COIN FLIP
+
+
+import random
+
+
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+
+number= random.randint(0,1)
+
+if(number==1):
+  print("Heads")
+else:
+  print("Tails")
+
+
+#DAY 4.2 BANK ROULETTE
+
+#COMPLEX version
+import random
+
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+
+# Split string method
+namesAsCSV = input("Give me everybody's names, seperated by a comma. ")
+names = namesAsCSV.split(", ")
+
+length=len(names)
+random_gen=random.randint(0,length-1)
+variable=names[random_gen]
+print(f"{variable} is going to buy the meal today!")
+
+#Simple_version using choice on list
+
+import random
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+namesAsCSV = input("Give me everybody's names, seperated by a comma. ")
+names = namesAsCSV.split(", ")
+#using choice 👇
+random_gen=random.choice(names)
+print(f"{random_gen} is going to buy the meal today!")
+
+#DAY 4.3 Treasure Map
+
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+
+a=int(position[0])
+b=int(position[1])
+a=a-1
+b=b-1
+map[a][b]='X'
+
+
+print(f"{row1}\n{row2}\n{row3}")
+
+
+
+#DAY4 Project ROCK PAPER SCISSOR
+#My version and Complex version, The normal one! 
+import random
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+choice= int(input(" What do you choose? Type 0 for Rock, Type 1 for Paper or 2 for Scissors.\n"))
+
+
+
+
+if(choice==0):
+  print(rock)
+
+if(choice==1):
+  print(paper)
+
+if(choice==2):
+  print(scissors)
+
+computer=random.randint(0,2)
+print("computer choose:")
+if(computer==0):
+  print(rock)
+
+if(computer==1):
+  print(paper)
+
+if(computer==2):
+  print(scissors) 
+
+#result
+
+if(choice>2 or choice<0):
+  print("You entered invalid number!")
+
+elif(choice==0 and computer==1):
+  print("You loose")
+  
+elif(choice==1 and computer==2):
+   print("You loose")
+
+elif(choice==2 and computer==0):
+     print("You loose")
+
+elif(choice==computer):
+  print("You draw")
+
+else:
+      print("You win")
+      
+#Tutor and simple version using list
+
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+game_images = [rock, paper, scissors]
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(game_images[user_choice])
+
+computer_choice = random.randint(0, 2)
+print("Computer chose:")
+print(game_images[computer_choice])
+
+if user_choice >= 3 or user_choice < 0: 
+  print("You typed an invalid number, you lose!") 
+elif user_choice == 0 and computer_choice == 2:
+  print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+  print("You lose")
+elif computer_choice > user_choice:
+  print("You lose")
+elif user_choice > computer_choice:
+  print("You win!")
+elif computer_choice == user_choice:
+  print("It's a draw")
+ 
+
+
